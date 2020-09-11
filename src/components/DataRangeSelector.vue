046cef65bb33fc5fde39ae788a73b8aad4e64817
c-zv <zv.const@gmail.com>
2020-09-11 10:21:19 +0100

    Create DataRangeSelector component

---
+++ src/components/DataRangeSelector.vue
@@ -0,0 +1,21 @@
+<template>
+  <b-datepicker
+    placeholder="Click to select a date range"
+    show-week-number
+    locale="en-GB"
+    range
+    v-bind="$props"
+    v-on="$listeners"
+  >
+  </b-datepicker>
+</template>
+
+<script>
+export default {
+  name: 'DataRangeSelector'
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,22 @@
 <template>
-  <div class="home">
+  <div class="container">
+    <data-range-selector
+      v-model="datesRange"
+    />
   </div>
 </template>
 
 <script>
+import DataRangeSelector from '@/components/DataRangeSelector.vue'
 export default {
   name: 'Home',
   components: {
+    DataRangeSelector
+  },
+  data () {
+    return {
+      datesRange: []
+    }
   }
 }
 </script>
